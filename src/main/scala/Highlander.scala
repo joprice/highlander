555188e536bc8f8ead3a76052700f61e9069960f
Joseph Price <pricejosephd@gmail.com>
2015-12-04 08:02:24 -0500

    focus tab's window

--- src/main/scala/Highlander.scala
+++ src/main/scala/Highlander.scala
@@ -1,7 +1,8 @@
 
+import chrome.windows.Windows
 import chrome.tabs.Tabs
 import chrome.tabs.bindings.{TabQuery, Tab, ChangeInfo, UpdateProperties}
-//import chrome.tabs.bindings._
+import chrome.windows.bindings.{Window, UpdateOptions}
 import scala.scalajs.js
 import scala.concurrent.Future
 import scalajs.concurrent.JSExecutionContext.Implicits.queue
@@ -12,14 +13,31 @@ object Highlander extends js.JSApp {
   // the url used when a tab is first open, before the url has been set
   val NewTab = "chrome://newtab/"
 
+  def focusWindow(id: Window.Id): Future[Window] = {
+    Windows.get(id).flatMap { window =>
+      if (!window.focused)
+        Windows.update(id, UpdateOptions(focused = true))
+      else Future.successful(window)
+    }
+  }
+
+  def focusTab(tab: Tab): Future[Option[Tab]] = {
+    focusWindow(tab.windowId).flatMap { window =>
+      Tabs.update(tab.id, UpdateProperties(active = true))
+        .map(_.toOption)
+    }
+  }
+
+  def tabsWithUrl(url: String): Future[js.Array[Tab]] = Tabs.query(TabQuery(url = url: js.Any))
+
   def checkForExistingTab(id: Tab.Id, url: String): Future[Option[Tab]] = {
-    Tabs.query(TabQuery(url = url: js.Any)).flatMap { tabs =>
+    tabsWithUrl(url).flatMap { tabs =>
       if (tabs.size > 1) {
         // take a tab, where that tab is not the newly opened one
         val ids = (tabs.flatMap(_.id.toOption) - id).tail :+ id
-        Tabs.remove(ids).flatMap { _ =>
-          Tabs.update(tabs.head.id, UpdateProperties(active = true))
-            .map(_.toOption)
+        focusTab(tabs.head).map { tab =>
+          Tabs.remove(ids)
+          tab
         }
       } else Future.successful(None)
     }
@@ -28,7 +46,9 @@ object Highlander extends js.JSApp {
   def main(): Unit = {
     Tabs.onUpdated.listen { case (id: Tab.Id, changeInfo: ChangeInfo, tab: Tab) =>
       changeInfo.url.filterNot(_ == NewTab)
-        .foreach(checkForExistingTab(id, _))
+        .foreach {
+          checkForExistingTab(id, _)
+        }
     }
   }
 }
